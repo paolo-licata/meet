bd44d12f9abb52f4bda43b7c3443677eaae7a606
Paolo Licata <paolo_licata@outlook.com>
2024-07-31 18:47:29 +0200

    Added scenario 2: List of Suggestion is now available from input interaction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import EventList from './components/EventList.js';
+import CitySearch from './components/CitySearch.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
