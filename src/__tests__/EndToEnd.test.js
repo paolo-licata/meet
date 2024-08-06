c54a439236a5f4a436d0556894de98a266e04a68
Paolo Licata <paolo_licata@outlook.com>
2024-08-06 11:25:15 +0200

    End to End testing for feature 2

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,16 +1,33 @@
 import puppeteer from 'puppeteer';
 
-test('An event element is collapsed by default', async () => {
-	const browser = await puppeteer.launch();
+describe('show/hide event details', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
 
-	const page = await browser.newPage();
-	await page.goto('http://localhost:3000/');
+  afterAll(() => {
+    browser.close();
+  });
 
-	// if your event element has a different selector, use it instead of .event
-	await page.waitForSelector('.event');
+  test('An event element is collapsed by default', async () => {
+    const eventDetails = await page.$('.event .details');
+    expect(eventDetails).toBeNull();
+  });
 
-	// if your event's details have a different selector, use it instead of .event .details
-	const eventDetails = await page.$('.event .details');
-	expect(eventDetails).toBeNull();
-	browser.close();
+  test('User can expand an event to see details', async () => {
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .details');
+    expect(eventDetails).toBeDefined();
+  });
+
+	test('User can collapse an event to hide details', async () => {
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .details');
+    expect(eventDetails).toBeNull();
+  });
 });
